08db1ba7b25f1c59392773812587f92c3a341a76
SomratIIT <101197949+SomratIIT@users.noreply.github.com>
2024-12-21 17:06:53 +0600

    button working sucessfully

--- js/video.js
+++ js/video.js
@@ -1,10 +1,19 @@
-function  getTimeString(time)
-{   const hour = parseInt(time/3600) ;
-    const minutes = time %3600
-    const minute = parseInt(minutes/60) ;
-    const seconds = minutes %60
-    const second = seconds;
-    return `${hour} hour ${minute} minute ${second} second ago`;
+function getTimeString(time) {
+  const hour = parseInt(time / 3600);
+  const minutes = time % 3600
+  const minute = parseInt(minutes / 60);
+  const seconds = minutes % 60
+  const second = seconds;
+  return `${hour} hour ${minute} minute ${second} second ago`;
+}
+const removeActiveClass=()=>
+{
+  const buttons = document.getElementsByClassName('category-btn');
+  console.log(buttons);
+  for(let btn of buttons)
+  {
+    btn.classList.remove("active");
+  }
 }
 
 console.log('Kaj kortese');
@@ -12,84 +21,92 @@ console.log('Kaj kortese');
 // 1-Fetch ,Load and Show Categories on HTML
 
 // Create loadCategories
-const loadCategories=()=>{
-    // For Fetch the data 
-    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
-    .then(response=>response.json())
-    
-    .then(data=>displayCategories(data.categories))
-    .catch((error)=>console.log(error));
+const loadCategories = () => {
+  // For Fetch the data 
+  fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then(response => response.json())
+
+    .then(data => displayCategories(data.categories))
+    .catch((error) => console.log(error));
 
 };
-loadCategories();
-const loadVideos=()=>{
-    // For Fetch the data 
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
-    .then(response=>response.json())
-    
-    .then(data=>displayVideos(data.videos))
-    .catch((error)=>console.log(error));
+loadCategories();  // 1st Load
+const loadVideos = () => {
+  // For Fetch the data 
+  fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(response => response.json())
+
+    .then(data => displayVideos(data.videos))
+    .catch((error) => console.log(error));
 
 };
-loadVideos();
-const loadcategoryVideos = (id)=>{
- // alert(id);
+loadVideos();   // 2nd Load
+const loadcategoryVideos = (id) => {
+  // alert(id);
   fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
-    .then(response=>response.json())
-    
-    .then(data=>displayVideos(data.category))
+    .then(response => response.json())
+
+    .then(data =>{
+
+      // sobar active class k remove korbo
+removeActiveClass();
+
+      // id er class k active korbo
+      const activeBtn = document.getElementById(`btn-${id}`);
+      activeBtn.classList.add('active');
+      console.log(activeBtn);
+      displayVideos(data.category);
+    })
 }
 const cardDemo = {
-    "category_id": "1001",
-    "video_id": "aaaa",
-    "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
-    "title": "Shape of You",
-    "authors": [
-        {
-            "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
-            "profile_name": "Olivia Mitchell",
-            "verified": ""
-        }
-    ],
-    "others": {
-        "views": "100K",
-        "posted_date": "16278"
-    },
-    "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+  "category_id": "1001",
+  "video_id": "aaaa",
+  "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+  "title": "Shape of You",
+  "authors": [
+    {
+      "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+      "profile_name": "Olivia Mitchell",
+      "verified": ""
+    }
+  ],
+  "others": {
+    "views": "100K",
+    "posted_date": "16278"
+  },
+  "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
 };
 // Display Videos
-const displayVideos=(videos)=>{
- console.log(videos);
-const videoContainer = document.getElementById('videos');
-videoContainer.innerHTML=""
-if(videos.length==0)
-{
-  videoContainer.classList.remove('grid');
-  //  videoContainer.classlist.remove("grid");
-  videoContainer.innerHTML=`
+const displayVideos = (videos) => {
+  console.log(videos);
+  const videoContainer = document.getElementById('videos');
+  videoContainer.innerHTML = ""
+  if (videos.length == 0) {
+    videoContainer.classList.remove('grid');
+    //  videoContainer.classlist.remove("grid");
+    videoContainer.innerHTML = `
   
   <div class="min-h-[200px] w-full flex flex-col gap-5 justify-center items-center ">
   <img  src="assets/Icon.png"  />
   </div>
   <h2 class="text-center font-bold text-xl">No Content here!</h2>
   `;
-  return;
-}
-else{
-  videoContainer.classList.add('grid');
-}
-videos.forEach((VID)=>{
+    return;
+  }
+  else {
+    videoContainer.classList.add('grid');
+  }
+  videos.forEach((VID) => {
     console.log(VID);
     const card = document.createElement("div");
-    card.classList='card card-compact'
+    card.classList = 'card card-compact'
     card.innerHTML = `
     <figure class="h-[200px] relative">
     <img
       src=${VID.thumbnail}
       alt="Shoes" class="h-full w-full object-cover"/>
     
-    ${
-        VID.others.posted_date?.length==0 ? "" :`<span class="absolute text-white right-2 bottom-2 bg-black rounded  p-2 text-xs">${getTimeString(VID.others.posted_date)}</span>`
+    ${VID.others.posted_date?.length == 0 ? "" : `<span class="absolute text-white right-2 bottom-2 bg-black rounded  p-2 text-xs">${getTimeString(VID.others.posted_date)}</span>`
       }
     
       
@@ -103,7 +120,7 @@ videos.forEach((VID)=>{
     <div class="flex items-center ">
     <p class='text-gray-400'> ${VID.authors[0].profile_name} </p>
   
-   ${VID.authors[0].verified==true ?   '<img class="w-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png"  />'  : ""}
+   ${VID.authors[0].verified == true ? '<img class="w-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png"  />' : ""}
     </div>
     
     <p> ${VID.others.views}</p>
@@ -111,32 +128,32 @@ videos.forEach((VID)=>{
   </div>`
 
 
-  videoContainer.append(card);
-})
+    videoContainer.append(card);
+  })
 }
 
 
 
 // Crrate displayCategories
 
-const displayCategories=(categories)=>{
-    // add Data in HTML
-    const categoryContainer = document.getElementById('categories');
+const displayCategories = (categories) => {
+  // add Data in HTML
+  const categoryContainer = document.getElementById('categories');
   categories.forEach((Cat) => {
     // console.log(Cat);
 
     // Create Button
-const buttonContainer = document.createElement('div');
-buttonContainer.innerHTML=
-`
-   <button onclick='loadcategoryVideos(${Cat.category_id})' class="btn btn-large ">
-   ${Cat.category}
+    const buttonContainer = document.createElement('div');
+    buttonContainer.innerHTML =
+      `
+   <button id="btn-${Cat.category_id}" onclick='loadcategoryVideos(${Cat.category_id})' class="btn btn-large category-btn">
+   ${Cat.category} 
    </button>
   
 `
 
-// Add btn to catagory container
-categoryContainer.append(buttonContainer);
+    // Add btn to catagory container
+    categoryContainer.append(buttonContainer);
 
   });
 }
