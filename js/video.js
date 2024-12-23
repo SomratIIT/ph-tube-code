98e2a747f53dc4b56ef7a8a2a2018528be732db9
SomratIIT <101197949+SomratIIT@users.noreply.github.com>
2024-12-23 12:57:24 +0600

    resolved problem

--- js/video.js
+++ js/video.js
@@ -25,12 +25,15 @@ const loadCategories = () => {
   // For Fetch the data 
   fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
     .then(response => response.json())
-
+ 
     .then(data => displayCategories(data.categories))
-    .catch((error) => console.log(error));
+    // .catch((error) => console.log(error));
 
 };
-loadCategories();  // 1st Load
+loadCategories();  // 1st Load call kora hoiche
+
+// video load kora hocche
+
 const loadVideos = () => {
   // For Fetch the data 
   fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
@@ -58,24 +61,7 @@ removeActiveClass();
       displayVideos(data.category);
     })
 }
-const cardDemo = {
-  "category_id": "1001",
-  "video_id": "aaaa",
-  "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
-  "title": "Shape of You",
-  "authors": [
-    {
-      "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
-      "profile_name": "Olivia Mitchell",
-      "verified": ""
-    }
-  ],
-  "others": {
-    "views": "100K",
-    "posted_date": "16278"
-  },
-  "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-};
+
 // Display Videos
 const displayVideos = (videos) => {
   console.log(videos);
@@ -135,10 +121,11 @@ const displayVideos = (videos) => {
 
 
 // Crrate displayCategories
-
+// 2nd Kaj
 const displayCategories = (categories) => {
   // add Data in HTML
   const categoryContainer = document.getElementById('categories');
+  // 3rd kaj
   categories.forEach((Cat) => {
     // console.log(Cat);
 
